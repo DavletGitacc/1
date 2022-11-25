95f9bc5c27af7cbaa701f3b0f77f69402dbb5553
DavletGitacc <azimovdavlet460@gmail.com>
2022-11-25 16:59:46 +0600

    6

--- lessons/home6.py
+++ lessons/home6.py
@@ -1,31 +1,41 @@
 class Hero:
-    def __init__(self,speed,night_vision, teleport, invisible):
+    def init(self, fly=0, power=0,speed=0,ads=0):
+        self.__fly=fly
+        self.__power=power
+        self.__speed=speed
+        self.__ads=ads
+    def setfly(self, fly):
+        self.__fly = fly
+
+    def getfly(self):
+        return self.__fly
+
+    def setpower(self, power):
+        self.__power = power
+
+    def getpower(self):
+        return self.__power
+
+    def setspeed(self, speed):
         self.__speed = speed
-        self.__night_vision = night_vision
-        self.__teleport = teleport
-        self.__invisible = invisible
 
-        def speed(self , speed):
-            self.__speed = speed
-        def speed1(self):
-            return  self.__speed
-        def n_vis(self , night_vision):
-            self.__night_vision = night_vision
-        def n_vis1(self):
-            return self.__night_vision
-        def teleport(self, teleport):
-            self.__teleport = teleport
-        def teleport1(self):
-            return self.__teleport
-        def invis(self, invisible):
-            self.__invisible = invisible
-        def invis1(self):
-            return self.__invisible
+    def getspeed(self):
+        return self.__speed
 
-Hero(speed=100,night_vision=True,teleport=True,invisible=True)
-print(Hero.__str__(f"Hero:  "
-                   f"speed 100.  "
-                   f"night_vision True.  "
-                   f"invisible True."))
+    def setads(self, ads):
+        self.__ads = ads
 
+    def getads(self):
+        return self.__ads
+
+hero=Hero()
+hero.setspeed(100)
+hero.setfly(70)
+hero.setpower(60)
+hero.setads(65)
+print(f"скорость {hero.getspeed()}\n"
+      f"сила {hero.getpower()}\n"
+      f"летучесть {hero.getfly()}\n"
+      f"супер сила {hero.getads()}")
+print()
 

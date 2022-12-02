5b771c2e89d0a2907ff4d7b8f9a90006919a6575
DavletGitacc <azimovdavlet460@gmail.com>
2022-12-02 21:15:32 +0600

    7

--- lessons/HW7.py
+++ lessons/HW7.py
@@ -1,25 +1,34 @@
 class Bank:
-    def __init__(self, money:int,user):
-        self.__money=money
-        self.__user=user
+    def __init__(self, money: int, user):
+        self.__money = money
+        self.__user = user
+
     def setmoney(self, money):
-        self.__money=money
+        self.__money = money
+
     def getmoney(self):
         return self.__money
+
     def setname(self, user):
-        self.__user3=user
+        self.__user = user
+
     def getname(self):
         return self.__user
+
     def moneyX(self):
-        self.__money+=int(input("Введите сумму:"))
-    def _kill(self):
-        self.__money=0
+        self.__money += int(input("Введите сумму:"))
+
+    def __kill(self):
+        self.__money = 0
+
     def __jackpot(self):
-        self.__money*=10
-user=Bank(10000,'Oleg Arigato')
+        self.__money *= 10
+
+
+user = Bank(10000, 'Oleg Arigato')
 user.moneyX()
-print(f"moneX:{user.Bank._money}")
+print(f"moneX:{user.getmoney()}")
+user._Bank__kill()
+print(f"kill:{user.getmoney()}")
 user._Bank__jackpot()
-print(f"jackpot:{user._Bank__money}")
-user._kill()
-print(f"kill:{user._Bank__money}")
+print(f"jackpot:{user.getmoney()}")
